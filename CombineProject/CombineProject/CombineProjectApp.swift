e1da02a457e85df87de51479b003a6e2f59ff882
uunwon <chea2089@naver.com>
2024-06-21 09:54:18 +0900

    Set Project

---
+++ CombineProject/CombineProject/CombineProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineProjectApp.swift
+//  CombineProject
+//
+//  Created by uunwon on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineProject/CombineProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineProject
+//
+//  Created by uunwon on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
