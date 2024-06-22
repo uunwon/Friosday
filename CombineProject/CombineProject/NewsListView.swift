2ff6eefbfc08b64676636d83f0271a6a199a53af
uunwon <chea2089@naver.com>
2024-06-22 18:14:39 +0900

    SwiftUI 뷰 구성

---
+++ CombineProject/CombineProject/NewsListView.swift
@@ -0,0 +1,47 @@
+//
+//  NewsListView.swift
+//  CombineProject
+//
+//  Created by uunwon on 6/21/24.
+//
+
+import SwiftUI
+
+struct NewsListView: View {
+    @StateObject private var viewModel = NewsViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(viewModel.newsItems) { item in
+                    NavigationLink(destination: NewsDetailView(item: item)) {
+                        NewsRow(item: item)
+                    }
+                }
+                if viewModel.hasMoreData, !viewModel.isLoading {
+                    ProgressView()
+                        .onAppear {
+                            viewModel.loadMore()
+                        }
+                }
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("뉴스 검색 (\(viewModel.newsItems.count) 건)")
+        .searchable(text: $viewModel.searchQuery, prompt: "검색어를 입력하세요.")
+        .overlay(
+             Group {
+                if let errorMessage = viewModel.errorMessage {
+                   Text(errorMessage)
+                       .foregroundColor(.red)
+                       .padding()
+                }
+             }
+        )
+    }
+    
+}
+
+#Preview {
+    NewsListView()
+}
