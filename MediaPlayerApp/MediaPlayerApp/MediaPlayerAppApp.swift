5a624049afd5d698e9f8c484fce4c3f804cbc993
uunwon <chea2089@naver.com>
2024-07-05 13:19:22 +0900

    start MediaPlayerApp

---
+++ MediaPlayerApp/MediaPlayerApp/MediaPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaPlayerAppApp.swift
+//  MediaPlayerApp
+//
+//  Created by uunwon on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MediaPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
