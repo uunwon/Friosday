64a438e60db100a47c0caab2e565e0108b693049
uunwon <chea2089@naver.com>
2024-07-06 12:58:01 +0900

    set FileItem & UI

--- MediaPlayerApp/MediaPlayerApp/ContentView.swift
+++ MediaPlayerApp/MediaPlayerApp/ContentView.swift
@@ -8,17 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var searchText = ""
+    
+    let mediaFiles: [FileItem] = [
+        FileItem(name: "song", children: [
+            FileItem(name: "song"), FileItem(name: "song2")]),
+        FileItem(name: "video", children: [
+            FileItem(name: "video"), FileItem(name: "video2"), FileItem(name: "video3")])
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(mediaFiles, children: \.children) { item in
+                Text(item.description)
+            }
+            .navigationTitle("File")
+            .toolbar {
+                Button {
+                    // MARK: - 파일 추가 같은 작업 ..
+                } label: {
+                    Image(systemName: "ellipsis.circle")
+                }
+            }
         }
-        .padding()
+        .searchable(text: $searchText,
+                    placement: .navigationBarDrawer(displayMode: .always))
     }
 }
 
 #Preview {
     ContentView()
+         .preferredColorScheme(.dark)
 }
---
+++ MediaPlayerApp/MediaPlayerApp/FileItem.swift
@@ -0,0 +1,22 @@
+//
+//  FileItem.swift
+//  MediaPlayerApp
+//
+//  Created by uunwon on 7/6/24.
+//
+
+import Foundation
+
+struct FileItem: Hashable, Identifiable, CustomStringConvertible {
+    var id: Self { self }
+    var name: String
+    var children: [FileItem]? = nil
+    var description: String {
+        switch children {
+        case nil:
+            return "📄 \(name)"
+        case .some(let children):
+            return children.isEmpty ? "📂 \(name)" : "📁 \(name)"
+        }
+    }
+}
