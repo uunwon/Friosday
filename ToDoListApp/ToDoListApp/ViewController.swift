298260a16ec8cb31db0bfe0e7842ea7e8167a55f
uunwon <chea2089@naver.com>
2024-07-12 17:50:30 +0900

    set ToDoList

---
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by uunwon on 7/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .lemon
+    }
+
+
+}
+
