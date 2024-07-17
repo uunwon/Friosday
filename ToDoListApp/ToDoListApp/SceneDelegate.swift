033a87214abc74cadd367bdb4743e57d2b614141
uunwon <chea2089@naver.com>
2024-07-17 14:03:20 +0900

    테이블 뷰 추가

--- ToDoListApp/ToDoListApp/SceneDelegate.swift
+++ ToDoListApp/ToDoListApp/SceneDelegate.swift
@@ -17,10 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let navi = UINavigationController(rootViewController: ViewController())
         
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
-        window?.rootViewController = viewController
+        window?.rootViewController = navi
         window?.makeKeyAndVisible()
     }
 
--- ToDoListApp/ToDoListApp/ViewController.swift
+++ ToDoListApp/ToDoListApp/ViewController.swift
@@ -7,14 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "todoCell")
+        
+        return tableView
+    }()
+    
+    private lazy var tableViewConstraints: [NSLayoutConstraint] = {
+        let safeArea = view.safeAreaLayoutGuide
+        return [
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "TODO"
+        
         view.backgroundColor = .lemon
+        view.addSubview(tableView)
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        updateLayout()
+    }
+    
+    // MARK: - Methods
+    func updateLayout() {
+        NSLayoutConstraint.activate(tableViewConstraints)
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        return cell
+    }
 }
 
