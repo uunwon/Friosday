b058c83e17d4877a831ca5a1409b221cce884c93
uunwon <chea2089@naver.com>
2024-07-26 14:38:46 +0900

    사용자 프로필 이미지 업로드 추가

--- SociallyApp/SociallyApp/Accounts/ViewModels/AuthViewModel.swift
+++ SociallyApp/SociallyApp/Accounts/ViewModels/AuthViewModel.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseAuth
 import FirebaseFirestore
+import FirebaseStorage
 import AuthenticationServices
 import CryptoKit
 
@@ -26,6 +27,31 @@ class AuthViewModel: ObservableObject {
             try Auth.auth().signOut()
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    // MARK: - Profile Image
+    func uploadProfileImage(_ imageData: Data) {
+        let storageReference = Storage.storage().reference().child("\(UUID().uuidString)")
+        storageReference.putData(imageData, metadata: nil) { metadata, error in
+            if let error = error {
+                return
+            }
+            
+            storageReference.downloadURL { url, error in
+                if let imageURL = url,
+                   let user = Auth.auth().currentUser {
+                    let changeRequest = user.createProfileChangeRequest()
+                    changeRequest.photoURL = imageURL
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            print("\(error.localizedDescription)")
+                            return
+                        }
+                        self.user = Auth.auth().currentUser
+                    }
+                }
+            }
         }
     }
     
