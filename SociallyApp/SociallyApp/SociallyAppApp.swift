f2609d188f7e8760a631d1c6551a22e5c50b330e
uunwon <chea2089@naver.com>
2024-07-26 09:27:34 +0900

    Initialize SociallyApp

---
+++ SociallyApp/SociallyApp/SociallyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SociallyAppApp.swift
+//  SociallyApp
+//
+//  Created by uunwon on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SociallyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
