677e433150395eb0d2f1d2ca08495e9b1e984949
uunwon <chea2089@naver.com>
2024-07-26 15:07:37 +0900

    Analytics 추가

--- SociallyApp/SociallyApp/SociallyAppApp.swift
+++ SociallyApp/SociallyApp/SociallyAppApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseAnalytics
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
@@ -39,6 +40,9 @@ struct SociallyAppApp: App {
             }
             .environmentObject(authModel)
             .environmentObject(PostViewModel())
+            .onAppear {
+                Analytics.logEvent("launch_app", parameters: nil)
+            }
         }
     }
 }
