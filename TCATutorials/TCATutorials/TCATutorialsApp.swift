c0e5cae39df4850db15c9e300d01099e092a8263
uunwon <chea2089@naver.com>
2024-07-30 15:05:54 +0900

    Initialize TCA Tutorials

---
+++ TCATutorials/TCATutorials/TCATutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialsApp.swift
+//  TCATutorials
+//
+//  Created by uunwon on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
