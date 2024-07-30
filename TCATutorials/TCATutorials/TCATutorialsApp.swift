6291377b9b81fde5538938fa0b7d92b1f237ac0c
uunwon <chea2089@naver.com>
2024-07-30 16:49:37 +0900

    Integrating into the app

--- TCATutorials/TCATutorials/TCATutorialsApp.swift
+++ TCATutorials/TCATutorials/TCATutorialsApp.swift
@@ -10,12 +10,15 @@ import SwiftUI
 
 @main
 struct TCATutorialsApp: App {
+    // 앱을 구동하는 store 는 한 번만 생성해야 한다는 점을 유의하기
+    static let store = Store(initialState: CounterFeature.State()) { 
+        CounterFeature()
+            ._printChanges()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            CounterView(store: Store(initialState: CounterFeature.State()) {
-                    CounterFeature()
-                }
-            )
+            CounterView(store: TCATutorialsApp.store)
         }
     }
 }
