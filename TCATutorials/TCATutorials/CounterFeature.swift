c8e257bb82d33de59746ebe6f9973ac296038ede
uunwon <chea2089@naver.com>
2024-07-30 15:49:41 +0900

    Create a reducer

---
+++ TCATutorials/TCATutorials/CounterFeature.swift
@@ -0,0 +1,35 @@
+//
+//  CounterFeature.swift
+//  TCATutorials
+//
+//  Created by uunwon on 7/30/24.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct CounterFeature {
+    @ObservableState
+    struct State {
+        var count = 0
+    }
+    
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+                
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            }
+        }
+    }
+}
